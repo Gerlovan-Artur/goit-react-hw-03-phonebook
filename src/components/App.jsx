62cf72faf8e3f462e65bac91ced378c1239a46d8
Gerlovan-Artur <gerlovanartur@gmail.com>
2022-12-25 14:46:50 +0200

    Done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,6 +16,20 @@ export class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const savedContacts = localStorage.getItem('contacts');
+    if (savedContacts !== null) {
+      this.setState({ contacts: JSON.parse(savedContacts),
+       });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   formSubmitHandler = (name, number) => {
     const findContact = this.state.contacts.find(contact => {
       return contact.name === name;
